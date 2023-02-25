3ac03b0983068bdc58d33b9f1c908bc5a46aa993
Josh Patra <joshpatra12@gmail.com>
2023-02-25 18:03:20 -0500

    email auth instead of google

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ruswipeshare/home.dart';
 
 import 'firebase_options.dart';
-import 'authGate.dart';
+import 'auth_gate.dart';
 
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(
-   options: DefaultFirebaseOptions.android,
- );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
