f6a313ebf3f95751b148af4194dd0612e687be01
NATHANIEL ENDICK <ne151@scarletmail.rutgers.edu>
2023-02-26 00:04:06 -0500

    buy ui

--- lib/buy.dart
+++ lib/buy.dart
@@ -12,16 +12,23 @@ class _BuyScreenState extends State<BuyScreen> {
   Widget build(BuildContext context) {
     TimeOfDay _time = TimeOfDay.now();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sell'),
-        automaticallyImplyLeading: false,
-      ),
-      body: OffersListView(),
+      body: const OffersListView(),
     );
   }
 }
 
-final List<String> entries = <String>['A', 'B', 'C'];
+final List<String> entries = <String>[
+  'Antoinette Beauchamp',
+  'Iliana Campbell',
+  'Angelique Straub',
+  'Ryleigh Pond',
+  'Andy Watters',
+  'Raphael Gossett',
+  'Kent Deutsch',
+  'Bridger Mojica',
+  'Pearl Morse',
+  'Jana Munguia'
+];
 final List<int> colorCodes = <int>[600, 500, 100];
 
 class OffersListView extends StatelessWidget {
@@ -30,14 +37,73 @@ class OffersListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        padding: const EdgeInsets.all(8),
         itemCount: 30,
         itemBuilder: (BuildContext context, int index) {
           return Container(
             height: 80,
-            color: Colors.amber[colorCodes[index % 3]],
-            child: Center(child: Text('Entry ${entries[index % 3]}')),
+            color: Colors.blue,
+            margin: const EdgeInsets.only(top: 4, bottom: 4),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 7,
+                  child: Container(
+                    color: Colors.red,
+                    margin: EdgeInsets.only(top: 4, left: 4, bottom: 4),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          entries[index % entries.length],
+                          textAlign: TextAlign.start,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontSize: 24),
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.star, size: 16),
+                            Icon(Icons.star, size: 16),
+                            Icon(Icons.star, size: 16),
+                            Icon(Icons.star_half, size: 16),
+                            Icon(Icons.star_border, size: 16),
+                          ],
+                        ),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              const Text(
+                                '\88:88PM - 88:88PM',
+                                textAlign: TextAlign.start,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    color: Colors.orange,
+                    child: const Text(
+                      '\$88',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 44),
+                    ),
+                  ),
+                )
+              ],
+            ),
           );
         });
   }
 }
+// return Container(
+//   height: 80,
+//   color: Colors.amber[colorCodes[index % colorCodes.length]],
+//   child: Center(child: Text(entries[index % entries.length])),
+// );
